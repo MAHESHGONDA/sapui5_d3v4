2f25076ebccdd58532e63311edf2cdd1b7ab2910
MAHESH GONDA KS <MAHESHMALKNOOR@GMAIL.COM>
2019-05-30 10:44:55 +0530

    Add files via upload

---
+++ Component.js
@@ -0,0 +1,31 @@
+sap.ui.define([
+  "sap/ui/core/UIComponent",
+  "sap/ui/model/json/JSONModel",
+  "sap/ui/model/odata/v2/ODataModel"
+], function(UIComponent, JSONModel, ODataModel) {
+  "use strict";
+  return UIComponent.extend("com.d3.demo.Component", {
+    metadata: {
+      // instantiates the resource model thanks to the app descriptor
+      manifest: "json"
+    },
+
+    init: function() {
+      // call the init function of the parent
+      UIComponent.prototype.init.apply(this, arguments);
+
+      // set local data model (schedule.json)
+      var oConfig = this.getMetadata().getConfig();
+      var sNamespace = this.getMetadata().getManifestEntry("sap.app").id;
+      
+      // mapping to the property "modelLocal" in the "config" property of the app descriptor
+      var oLocalModel = new JSONModel(jQuery.sap.getModulePath(sNamespace, oConfig.modelLocal));
+      this.setModel(oLocalModel);
+
+
+
+      // create the views based on the url/hash
+      this.getRouter().initialize();
+    }
+  });
+});
